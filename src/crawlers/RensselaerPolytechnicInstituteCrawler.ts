0433fc3f263e5f7c6d7375407f52a60085d63f20
Josh Goldberg <josh@fullscreenmario.com>
2016-09-27 15:47:25 -0700

    Semi finalized Crawler.ts

--- src/crawlers/RensselaerPolytechnicInstituteCrawler.ts
+++ src/crawlers/RensselaerPolytechnicInstituteCrawler.ts
@@ -3,6 +3,12 @@
 import { Crawler } from "../crawler";
 import { PhantomJS, WebPage } from "phantom";
 
+function wait(milliseconds: number): Promise<void> {
+    return new Promise<void>((resolve => {
+        setTimeout(resolve, milliseconds);
+    }));
+}
+
 export class RensselaerPolytechnicInstituteCrawler extends Crawler {
     /**
      * 
@@ -23,7 +29,14 @@ export class RensselaerPolytechnicInstituteCrawler extends Crawler {
      */
     protected crawlLandingPage(webPage: WebPage, landingPage: string): Promise<void> {
         console.log("rpi crawling");
+
+        return wait(1000)
+            .then(() => webPage.evaluate(function () {
+                return document.body.querySelectorAll(".club-box-title-span");
+            }))
+            .then((stuff): void => {
+                console.log("sup", stuff);
+            });
         // idea: continuously scroll down until the page height doesn't increase
-        return Promise.resolve();
     }
 }
