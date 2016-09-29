da2863b80d19187ca79a725a2626bf1e1a5007a3
Ian Craig (SWAY) <iancra@microsoft.com>
2016-09-28 17:40:46 -0700

    Csv formatter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { OhioStateCrawler } from "./crawlers/implementations/OhioStateCrawler";
 import { IOrganization } from "./models/IOrganization";
+import { CsvOrganizationFormatter } from "./formatters/CsvOrganizationFormatter";
 
 ((): void => {
     new OhioStateCrawler()
         .crawl()
         .then((organization: IOrganization): void => {
             console.log("org", JSON.stringify(organization));
+            console.log(CsvOrganizationFormatter.OutputGroups(organization));
         });
 })();
